04383428932780ff550df04dbe46903e90c2563b
NofarShlosberg <Nofar740@gmail.com>
2023-01-22 16:43:18 +0200

    Fill login function

--- app/src/main/java/com/example/finalproject/viewmodel/AuthenticationViewModel.java
+++ app/src/main/java/com/example/finalproject/viewmodel/AuthenticationViewModel.java
@@ -11,13 +11,27 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AuthenticationViewModel extends ViewModel {
 
 
-    public void login(String email, String password) {
-
+    public void login(String email, String password, AuthCallback callback) {
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        callback.consume(authResult);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        callback.onException(e);
+                    }
+                });
     }
 
     public void register(User user, String password, AuthCallback callback) {
@@ -31,8 +45,16 @@ public class AuthenticationViewModel extends ViewModel {
                         // create a document for the user with new details
                         FirebaseFirestore
                                 .getInstance()
-                                        .collection(usersCollection)
-                                                .add(user);
+                                .collection(usersCollection)
+                                .add(user)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        // update the user object's id to the firebase-created id
+                                        String newId = documentReference.getId();
+                                        documentReference.update("id",newId);
+                                    }
+                                });
                         callback.consume(authResult);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
