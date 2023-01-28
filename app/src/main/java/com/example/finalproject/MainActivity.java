9fd005a441a734517895e5872525b2cb2a7abf1a
NofarShlosberg <Nofar740@gmail.com>
2023-01-28 14:30:10 +0200

    Added navigation component

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.finalproject;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.material.navigation.NavigationView;
 
-import java.util.function.Consumer;
 
 public class MainActivity extends BaseActivity {
 
+
+    private AppBarConfiguration appBarConfiguration;
+    private NavigationView navigationView;
+    private NavController navController;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show();
+        navigationView = findViewById(R.id.nav_MAIN_view);
+        drawer = findViewById(R.id.drawer);
+        navController = Navigation.findNavController(this,R.id.nav_MAIN_host);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+                .setDrawerLayout(drawer)
+                .build();
+        NavigationUI.setupWithNavController(navigationView,navController);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
 
+    }
 }
---
+++ app/src/main/java/com/example/finalproject/view/main/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.example.finalproject.view.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finalproject.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
+
