7066c3d463f40ac4faf70a72a9297453535e96c4
NofarShlosberg <Nofar740@gmail.com>
2023-02-04 14:21:43 +0200

    Add login Screen & TextView,EditText,Button styles in xml

--- app/src/main/java/com/example/finalproject/view/auth/AuthActivity.java
+++ app/src/main/java/com/example/finalproject/view/auth/AuthActivity.java
@@ -1,8 +1,16 @@
 package com.example.finalproject.view.auth;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AuthActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
